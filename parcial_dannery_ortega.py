e72cf079f64af281e30f5571ea28dfcc7b004a1e
DanneryOrtega <dannery19ortega18@gmail.com>
2021-03-18 23:08:54 -0500

    pr commit

--- parcial_dannery_ortega.py
+++ parcial_dannery_ortega.py
@@ -1,10 +1,11 @@
 
-
-texto = """este es un texto el cual deben contar el numero
-de palabras que tiene, deben tener en cuenta,
-que algunas palabras se separa por un punto, y una
-coma, tambien hay que tener en cuenta, que las palabras
-escritas EN MAYUSCULAS y minusculas cuenta como una este. Texto"""
+texto=input("Ingrese el texto: ")
+buscar=input("Que frecuencia hay en el texto de la palabra: ")
+# texto = """este es un texto el cual deben contar el numero
+# de palabras que tiene, deben tener en cuenta,
+# que algunas palabras se separa por un punto, y una
+# coma, tambien hay que tener en cuenta, que las palabras
+# escritas EN MAYUSCULAS y minusculas cuenta como una este. Texto"""
 
 no_valido = ",;:."
 for caracter in no_valido:
@@ -15,5 +16,8 @@ texto = texto.lower()
 palabras = texto.split(" ")
 
 texto="Contando cuantas letras tiene este texto"
-re=palabras.count("un")
+re=palabras.count(buscar)
+tex="La frecuencia de la palabra ("
+r=") es: "
+print(tex+buscar+r)
 print(re)
